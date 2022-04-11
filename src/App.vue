404fd4ea6d7d8784385c9d262cbae24b556204d9
sundlee <sundlee@ncsoft.com>
2022-04-11 16:47:21 +0900

    explorer 헤더 영역 추가 + 파일 트리 배경 변경

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,96 @@
 <template>
-  <div id="app">
+  <div id="app" @click="contextMenuIsVisible = false">
     <nav-bar />
+    <div class="last-event row">
+      Last event: {{ lastEvent }}
+    </div>
     <div class="split">
       <div id="split-0" class="content">
         <b-card class="aside">
           <template #header>
             <div class="explorer-header">
               <h6 class="mb-0">EXPLORER</h6>
-              <b-button
-                variant="custom-4"
-                size="sm"
-                @click="addNode"
+              <b-dropdown
+                ref="plusDropdownBtn"
+                right
+                variant="link"
+                class="plus-button"
+                no-caret
               >
-                <b-icon icon="plus"></b-icon>
-              </b-button>
+                <template #button-content>
+                  <span class="btn-more text-dark">
+                    <b-icon icon="plus"></b-icon>
+                  </span>
+                </template>
+                <b-dropdown-item-button @click.prevent.stop="addFile">
+                  새 파일
+                </b-dropdown-item-button>
+                <b-dropdown-item-button @click.prevent.stop="addFolder">
+                  새 폴더
+                </b-dropdown-item-button>
+                <b-dropdown-divider></b-dropdown-divider>
+                <b-dropdown-item-button @click.prevent.stop="importZip">
+                  zip 파일
+                </b-dropdown-item-button>
+              </b-dropdown>
             </div>
           </template>
-          <!-- <button
-            class="vtl-btn"
-            @click="addNode"
+<!-- -->
+          <sl-vue-tree
+            v-model="nodes"
+            ref="slVueTree"
+            :allow-multiselect="false"
+            @select="nodeSelected"
+            @drop="nodeDropped"
+            @toggle="nodeToggled"
+            @nodecontextmenu="showContextMenu"
           >
-            Add Node
-          </button> -->
-          <vue-tree-list
-            @click="onClick"
-            @change-name="onChangeName"
-            @delete-node="onDel"
-            @add-node="onAddNode"
-            :model="data"
-            default-tree-node-name="new node"
-            default-leaf-node-name="new leaf"
-            v-bind:default-expanded="false"
-          >
-            <!-- <template v-slot:leafNameDisplay="slotProps">
-              <span>
-                {{ slotProps.model.name }} <span class="muted">#{{ slotProps.model.id }}</span>
+            <template slot="title" slot-scope="{ node }">
+              <span class="item-icon">
+                <i class="fa fa-file" v-if="node.isLeaf"></i>
+                <i class="fa fa-folder" v-if="!node.isLeaf"></i>
               </span>
-            </template> -->
-            <!-- <span class="icon" slot="addTreeNodeIcon">📂</span>
-            <span class="icon" slot="addLeafNodeIcon">＋</span>
-            <span class="icon" slot="editNodeIcon">📃</span>
-            <span class="icon" slot="delNodeIcon">✂️</span>
-            <span class="icon" slot="leafNodeIcon">🍃</span>
-            <span class="icon" slot="treeNodeIcon">🌲</span> -->
-          </vue-tree-list>
-          <!-- <button @click="getNewTree">Get new tree</button>
-          <pre>
-            {{newTree}}
-          </pre> -->
+              {{ node.title }}
+            </template>
+
+            <template slot="toggle" slot-scope="{ node }">
+              <span v-if="!node.isLeaf">
+                <i v-if="node.isExpanded" class="fa fa-chevron-down"></i>
+                <i v-if="!node.isExpanded" class="fa fa-chevron-right"></i>
+              </span>
+            </template>
+
+            <template slot="draginfo">
+              {{ selectedNodesTitle }}
+            </template>
+          </sl-vue-tree>
+<!-- -->
         </b-card>
       </div>
       <div id="split-1" class="content">
-        <codemirror
+        <div class="json-preview">
+          <pre>{{ JSON.stringify(nodes, null, 4)}}</pre>
+        </div>
+        <div class="contextmenu" ref="contextmenu" v-show="contextMenuIsVisible">
+          <div @click="removeNode">Remove</div>
+        </div>
+        <!-- <codemirror
           ref="codemirror"
           :value="code"
           :options="cmOptions"
-        />
+        /> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Split from 'split.js';
-import NavBar from './components/NavBar.vue';
-import { VueTreeList, Tree, TreeNode } from '@/components/vtl/'
-import { codemirror } from 'vue-codemirror';
+import JSZip from 'jszip';
+import { saveAs } from '@/utils/file-saver';
+import SlVueTree  from '@/components/sl-vue-tree/sl-vue-tree.vue';
+// import { codemirror } from 'vue-codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/addon/lint/lint.css';
 import 'codemirror/lib/codemirror';
@@ -73,67 +98,36 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
 import 'codemirror/addon/lint/json-lint';
 import 'codemirror/addon/selection/active-line';
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    VueTreeList,
-    codemirror,
+    SlVueTree,
+    // codemirror,
   },
   data() {
     return {
-      newTree: {},
-      data: new Tree([
+      nodes: [
+        { title: 'Item1', isLeaf: true },
+        { title: 'Item2', isLeaf: true },
+        { title: 'Folder1' },
         {
-          name: 'Node 1',
-          id: 1,
-          // pid: 0,
-          // dragDisabled: true,
-          // addTreeNodeDisabled: true,
-          // addLeafNodeDisabled: true,
-          // editNodeDisabled: true,
-          // delNodeDisabled: true,
-          children: [
+          title: 'Folder2', isExpanded: true, children: [
+            { title: 'Item3', isLeaf: true },
+            { title: 'Item4', isLeaf: true },
             {
-              name: 'Node 1-2',
-              id: 2,
-              isLeaf: true,
-              pid: 1
+              title: 'Folder3', children: [
+                { title: 'Item5', isLeaf: true }
+              ]
             }
           ]
         },
-        {
-          name: 'Node 2',
-          id: 3,
-          pid: 0,
-          // disabled: true
-        },
-        {
-          name: 'Node 3',
-          id: 4,
-          pid: 0
-        },
-        {
-          name: 'Node 4',
-          id: 5,
-          pid: 0,
-          children: [
-            {
-              name: 'Node 4-1',
-              id: 6,
-              isLeaf: true,
-              pid: 0
-            },
-            {
-              name: 'Node 4-2',
-              id: 7,
-              isLeaf: true,
-              pid: 7
-            }
-          ]
-        }
-      ]),
+      ],
+      selectedNodesTitle: '',
+      contextMenuIsVisible: false,
+      lastEvent: 'No last event',
 
       code: 'console.log(\'Hello World 1!\');\n\nconsole.log(\'Hello World 2!\');\n',
       cmOptions: {
@@ -158,6 +152,8 @@ export default {
     };
   },
   mounted() {
+    window.slVueTree = this.$refs.slVueTree;
+
     let sizes = localStorage.getItem('split-sizes')
     if (sizes) {
       sizes = JSON.parse(sizes);
@@ -178,23 +174,23 @@ export default {
     });
   },
   methods: {
-    onDel(node) {
-      console.log('onDel: ', node)
-      node.remove()
+    // onChangeName(params) {
+    //   console.log('onChangeName: ', params);
+    // },
+    // onAddNode(params) {
+    //   console.log('onAddNode: ', params);
+    // },
+    // onClick(params) {
+    //   console.log('onClick: ', params);
+    // },
+    addFile() {
+      console.log('addFile is called.');
     },
-    onChangeName(params) {
-      console.log('onChangeName: ', params)
+    addFolder() {
+      console.log('addFolder is called.');
     },
-    onAddNode(params) {
-      console.log('onAddNode: ', params)
-    },
-    onClick(params) {
-      console.log('onClick: ', params)
-    },
-    addNode() {
-      var node = new TreeNode({ name: 'new node', isLeaf: false })
-      if (!this.data.children) this.data.children = []
-      this.data.addChildren(node)
+    importZip() {
+      console.log('importZip is called.');
     },
     getNewTree() {
       var vm = this
@@ -217,17 +213,168 @@ export default {
       }
 
       vm.newTree = _dfs(vm.data);
-    }
+    },
+    handleFileUpload() {
+      this.files = [];
+      this.fileNames = [];
+      this.selectedFile = this.$refs.file.files[0];
+      // console.log(this.selectedFile.name);
+
+      var new_zip = new JSZip();
+      new_zip.loadAsync(this.selectedFile)
+        .then((zip) => {
+          this.rawFileNames = Object.keys(zip.files);
+          console.log(this.rawFileNames);
+          
+          const promiseArray = [];
+          this.rawFileNames.forEach((file) => {
+            if (!file.endsWith('/')) {
+              this.fileNames.push(file);
+              promiseArray.push(zip.file(file).async('uint8array'));
+            }
+          });
+          Promise.all(promiseArray)
+            .then((data) => {
+              // console.log(data);
+              this.fileNames.forEach((file, idx) => {
+                this.files.push({
+                  name: file,
+                  content: data[idx],
+                });
+              });
+              // console.log(`files: ${JSON.stringify(this.files, null, 4)}`);
+            });
+        });
+    },
+    handleClickDownloadBtn() {
+      var zip = new JSZip();
+
+      this.files.forEach((file) => {
+        zip.file(file.name, file.content);
+      });
+
+      zip.generateAsync({ type: "blob" }, function updateCallback(metadata) {
+          var msg = "progression: " + metadata.percent.toFixed(2) + " %";
+          if(metadata.currentFile) {
+              msg += ", current file = " + metadata.currentFile;
+          }
+          console.log(msg);
+      })
+      .then(function callback(blob) {
+          saveAs(blob, "example.zip");
+
+          console.log("done !");
+      }, function (e) {
+          console.log(e);
+      });
+    },
+    toggleVisibility: function (event, node) {
+      const slVueTree = this.$refs.slVueTree;
+      event.stopPropagation();
+      const visible = !node.data || node.data.visible !== false;
+      slVueTree.updateNode(node.path, {data: { visible: !visible}});
+      this.lastEvent = `Node ${node.title} is ${ visible ? 'visible' : 'invisible'} now`;
+    },
+    nodeSelected(nodes, event) {
+      console.log(nodes);
+      this.selectedNodesTitle = nodes.map(node => node.title).join(', ');
+      this.lastEvent = `Select nodes: ${this.selectedNodesTitle}`;
+    },
+    nodeToggled(node, event) {
+      this.lastEvent = `Node ${node.title} is ${ node.isExpanded ? 'expanded' : 'collapsed'}`;
+    },
+    nodeDropped(nodes, position, event) {
+      this.lastEvent = `Nodes: ${nodes.map(node => node.title).join(', ')} are dropped ${position.placement} ${position.node.title}`;
+    },
+    showContextMenu(node, event) {
+      event.preventDefault();
+      this.contextMenuIsVisible = true;
+      const $contextMenu = this.$refs.contextmenu;
+      $contextMenu.style.left = event.clientX + 'px';
+      $contextMenu.style.top = event.clientY + 'px';
+    },
+    removeNode() {
+      this.contextMenuIsVisible = false;
+      const $slVueTree = this.$refs.slVueTree;
+      const paths = $slVueTree.getSelected().map(node => node.path);
+      $slVueTree.remove(paths);
+    },
   },  
 };
 </script>
 
+<style src="@/components/sl-vue-tree/sl-vue-tree-code.css"></style>
 <style lang="scss">
+body {
+  background: #050d12;
+  font-family: Arial;
+  color: rgba(255, 255, 255, 0.5);
+}
+
+a {
+  color: #bbb;
+}
+
+.row {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.contextmenu {
+  position: absolute;
+  background-color: white;
+  color: black;
+  border-radius: 2px;
+  cursor: pointer;
+}
+
+.contextmenu > div {
+  padding: 10px;
+}
+
+.contextmenu > div:hover {
+  background-color: rgba(100, 100, 255, 0.5);
+}
+
+
+.last-event {
+  color: white;
+  background-color: rgba(100, 100, 255, 0.5);
+  padding: 10px;
+  border-radius: 2px;
+}
+
+.tree-container {
+  flex-grow: 1;
+}
+
+.sl-vue-tree.sl-vue-tree-root {
+  flex-grow: 1;
+  overflow-x: hidden;
+  overflow-y: auto;
+  height: 300px;
+}
+
+
+.json-preview {
+  // flex-grow: 1;
+  // margin-left: 10px;
+  // background-color: #13242d;
+  // border: 1px solid black;
+  padding: 10px;
+  font-size: 13px;
+}
+
+.item-icon {
+  display: inline-block;
+  text-align: left;
+  width: 20px;
+}
+
 .split {
   display: flex;
   flex-direction: row;
   .content {
-    // padding: 8px;
     border: 1px solid #c0c0c0;
     box-shadow: inset 0 1px 2px #e4e4e4;
     background-color: #fff;
@@ -240,7 +387,20 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
+        .plus-button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .dropdown-menu.show {
+            width: 110px;
+            left: -16px !important;
+            font-size: 13px;
+          }
+        }
       }
+    }
+    .card-body {
+      padding: 0;
     }
   }
 }
@@ -266,4 +426,5 @@ export default {
   color: gray;
   font-size: 80%;
 }
+
 </style>
